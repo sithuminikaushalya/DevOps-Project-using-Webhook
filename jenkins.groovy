1a61b215066322143428ea366c569ccf132a7216
Chathurya Prasad <121708803+CPrasa@users.noreply.github.com>
2024-06-29 12:12:12 +0530

    chnages

---
+++ jenkins.groovy
@@ -0,0 +1,47 @@
+pipeline {
+    agent any
+
+    triggers {
+        githubPush()
+    }
+
+    environment {
+        REPO_URL = 'https://github.com/CPrasa/DevOps-Project'
+        BRANCH = 'main'
+        DOCKER_REGISTRY = 'crprasad'
+        APP_NAME = 'RMS'
+        PORT = '3000'
+    }
+
+    stages {
+        stage('Clone Repository') {
+            steps {
+                git branch: "${BRANCH}", url: "${REPO_URL}"
+            }
+        }
+
+        stage('Build Docker Images') {
+            steps {
+                script {
+                    bat 'docker-compose build'
+                }
+            }
+        }
+
+        stage('Push Docker Images') {
+            steps {
+                script {
+                    bat 'docker-compose push'
+                }
+            }
+        }
+        stage('Deploy Application') {
+            steps {
+                script {
+                    bat 'docker-compose down'
+                    bat 'docker-compose up -d'
+                }
+            }
+        }
+    }
+}
